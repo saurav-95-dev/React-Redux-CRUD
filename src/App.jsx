4a3ffd2749047241ce28468a3ed20cf3178700a2
Saurabh Dev <devsaurabh086@gmail.com>
2025-04-17 11:46:45 +0530

    useState-1-Demo-1-Revisit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
 import { useState } from "react";
 
-export default function App() {
 
-  //Demo-1
+export default function(){
+
+  const [ingredients , setIngredients] = useState(["Apple" , "Guava" , "pinapple" , "grapes"]);
   
-  const [ingredients , setIngredients] = useState(["Apple" , "Guava" , "Orange" , "pinapple"]);
-
   const handleClick=(event)=>{
-     
+     console.log("Handle click function is running...!");
      event.preventDefault();
-     const formData = new FormData(event.currentTarget);
-     const item = formData.get("new-fruit");
-    
+     const formData  = new FormData(event.currentTarget);
+     const newItem  = formData.get("new-item");
+     console.log(newItem);
      setIngredients((prev)=>{
-        return [...prev , item];
+        return [...prev , newItem];
      })
 
-     console.log("handleClick function is running and Form got submitted..!");
-     console.log(ingredients);
   }
 
-
-
-
-
-  return (
+  return(
     <>
-     
-      <main>
-        {console.log("Return is running")}
-        <h1>useState</h1>
-        <form onSubmit={handleClick}>
-          <input type="text" placeholder="Search fruits" name="new-fruit" />
-          <button>+Add Ingredients</button>
-        </form>
-        <ul>
-         {ingredients.map((ingredient , index)=>{
-          return <li key={index}>{ingredient}</li> //Here we are returning jsx list items !
-         })}
-        
-        </ul>
-      </main>
-
+    {console.log("return of app component is running..!")};
+    {console.log(ingredients)}
+    <h1>useState demo-1</h1>
+    <main>
+      <form onSubmit={handleClick}>
+        <input type="text" placeholder="Search fruits" name="new-item" />
+        <button>Click here</button>
+      </form>
+      <ul>
+        {
+          ingredients.map((ingredient , index)=>{
+            return <li key={index}>{ingredient}</li>
+          })
+        }
+      </ul>
+    </main>
     </>
+    
   )
+
 }
