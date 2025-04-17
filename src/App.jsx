e32e9234386e38b53b07abf96ae8332b8e4c40ab
Saurabh Dev <devsaurabh086@gmail.com>
2025-04-17 12:04:08 +0530

    useState1-on-boolean-Demo-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import { useState } from "react"
 
 
 
-export default function App(){
+export default function(){
+   
+  const [val , setVal] = useState(false);
+  
+  const handleClick=()=>{
+    setVal((prev)=>{
+      return !prev;
+   })
+ 
+  }
 
-   const [count , setCount] = useState(0);
+  let str = val ? "This is ture" : "This is false"
+  return(
+    <>
+    <h2>useState demo-3</h2>
+    <h1>{str}</h1>
+    <button onClick ={handleClick}>Press to flip</button>
+    </>
+  )
 
-   const increaseValue=()=>{
-       setCount((prev)=>{
-          return prev + 1;
-       })
-   }
-
-   const decreaseValue=()=>{
-      setCount((prev)=>{
-        return prev-1;
-      })
-   }
-   return(
-       <>
-       <h2>useState demo-2</h2>
-       <h1>{count}</h1>
-       <button onClick={increaseValue}>+Increase</button>
-       <button onClick = {decreaseValue}>-Decrease</button>
-       </>
-   )
 }
