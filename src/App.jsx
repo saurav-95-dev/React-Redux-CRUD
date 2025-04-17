6a77ea4f54e56e1a0936bacf7b3b3ab12a8ee04b
Saurabh Dev <devsaurabh086@gmail.com>
2025-04-17 11:54:05 +0530

    useState on integers Demo-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import { useState } from "react";
+import { useState } from "react"
 
 
-export default function(){
 
-  const [ingredients , setIngredients] = useState(["Apple" , "Guava" , "pinapple" , "grapes"]);
-  
-  const handleClick=(event)=>{
-     console.log("Handle click function is running...!");
-     event.preventDefault();
-     const formData  = new FormData(event.currentTarget);
-     const newItem  = formData.get("new-item");
-     console.log(newItem);
-     setIngredients((prev)=>{
-        return [...prev , newItem];
-     })
+export default function App(){
 
-  }
+   const [count , setCount] = useState(0);
 
-  return(
-    <>
-    {console.log("return of app component is running..!")};
-    {console.log(ingredients)}
-    <h1>useState demo-1</h1>
-    <main>
-      <form onSubmit={handleClick}>
-        <input type="text" placeholder="Search fruits" name="new-item" />
-        <button>Click here</button>
-      </form>
-      <ul>
-        {
-          ingredients.map((ingredient , index)=>{
-            return <li key={index}>{ingredient}</li>
-          })
-        }
-      </ul>
-    </main>
-    </>
-    
-  )
+   const increaseValue=()=>{
+       setCount((prev)=>{
+          return prev + 1;
+       })
+   }
 
+   const decreaseValue=()=>{
+      setCount((prev)=>{
+        return prev-1;
+      })
+   }
+   return(
+       <>
+       <h2>useState demo-2</h2>
+       <h1>{count}</h1>
+       <button onClick={increaseValue}>+Increase</button>
+       <button onClick = {decreaseValue}>-Decrease</button>
+       </>
+   )
 }
