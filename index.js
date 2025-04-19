641914a5abebb62709ead8fb222f1f2b5d26b8ea
Saurabh <devsaurabh086@gmail.com>
2025-04-19 23:45:49 +0530

    API data-fetch-async-await

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 //Promises : 
+let btn = document.getElementById("btn");
+let searchBar = document.getElementById("input-text");
+
 
 console.log("Hello")
 
-async function getData() {
-    let result = await fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
-    console.log(result);
+async function getData(value) {
+    const promise = await fetch(`https://jsonplaceholder.typicode.com/comments?id=${value}`)
+    return await promise.json();
     
 }
 
-
-getData();
+btn.addEventListener("click", async () => {
+    let val = searchBar.value;
+    const res = await getData(val);
+    console.log(res);
+})
 
 console.log("This is the line after getData");
 
