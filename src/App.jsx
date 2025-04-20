0fbee41cd68f1f02990b385659c11219a280b9bc
Saurabh <devsaurabh086@gmail.com>
2025-04-21 02:37:41 +0530

    Immutability in state (in nested object)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+//immutability in state (in object)
+
 import React, { useState } from "react";
 
 export default function App() {
-   
-  const [person, setPerson] = useState({ name: "Saurabh", age: 0, add: "North pole" });
-  
+
+  const [person, setPerson] = useState({
+    name: "Saurabh", age: 12, add: "North pole", Qualifications: {
+
+      degree: "Btech",
+      branch: "IT",
+      
+  } });
+ 
   function handleUpdate() {
-  
-    setPerson({...person , name : "swaha" , designation : "SDE1" , age : 340});
+    setPerson({...person , name : "Something" , age : 0 , Qualifications : {...person.Qualifications , degree : "Mtech" , branch : "CS"}})
   }
-  
+
   return (
     <>
-      <h1>name : {person.name}</h1>
-      <h1>age : {person.age}</h1>
-      <h1>add : {person.add}</h1>
-      <h1>designation : { person.designation}</h1>
+      <h1>{person.name}</h1>
+      <h1>{person.age}</h1>
+      <h1>{person.add}</h1>
+      <h1>{person.Qualifications.degree}</h1>
+      <h1>{person.Qualifications.branch}</h1>
       
       <button onClick={handleUpdate}>Update</button>
     </>
