e49b3571e638a9d7cbb78cd2268c5362f0f56cf1
Saurabh <devsaurabh086@gmail.com>
2025-04-20 20:43:24 +0530

    Immutability in state (with objects)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-//Immutaility in state (Arrays):
-
-import { useState } from "react"
+import React, { useState } from "react";
 
 export default function App() {
-
-  const [item, setItem] = useState(["Apple", "orange", "guava"]);
-
+   
+  const [person, setPerson] = useState({ name: "Saurabh", age: 0, add: "North pole" });
+  
   function handleUpdate() {
-    console.log("handleUpdate function is running .. ")
-    //item.push("new item"); //Directly changing the array
-    setItem([...item, "new Item"]);
+  
+    setPerson({...person , name : "swaha" , designation : "SDE1" , age : 340});
   }
-
+  
   return (
     <>
+      <h1>name : {person.name}</h1>
+      <h1>age : {person.age}</h1>
+      <h1>add : {person.add}</h1>
+      <h1>designation : { person.designation}</h1>
       
-      <ul>
-        {
-          item.map((fruits , index) => {
-            return <li key={index}>{ fruits}</li>
-          })
-        }
-      </ul>
       <button onClick={handleUpdate}>Update</button>
-
     </>
+    
   )
 }
