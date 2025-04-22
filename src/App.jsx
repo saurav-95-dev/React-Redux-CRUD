8662bef55d7c6dff0f9cf31cf4f5f7d5d6349814
Saurabh Dev <devsaurabh086@gmail.com>
2025-04-22 12:01:17 +0530

    API-calling with useEffect(empty dependency array)-->runs as componentDidMount only and clean-up(if exist) will run on component un-mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //Fetching data from an API:
 
 import React , {useState , useEffect }from "react";
@@ -8,11 +8,14 @@ import getData from "./api";
 export default function App(){
 
   const [userData , setUserData] = useState(null);
+  const [visible , setVisible] = useState(true);
 
   useEffect(()=>{
      getData().then((data)=>{
          
+     console.log("App wala useEffect is runnig .. ")
          setUserData(data.results[0]);
+
      })
   },[]) //Run as componentDidMount and component-unmount , It dont run on component state update
 
@@ -22,70 +25,25 @@ export default function App(){
      })
   }
 
+  function handleRemove(){
+     setVisible((prev)=>{
+        return !prev;
+     })
+  }
+
 
   return(
 
     <>
 
-    {userData ?  <MyComponent userData = {userData}/> : <>Pls Wait...</>}
+    {(userData && visible) ?  <MyComponent userData = {userData}/> : <>Pls Wait...</>}
     <br></br><br></br>
     <button className="refresh-btn" onClick={handleRefresh}>Refresh</button>
+    <br></br><br></br>
+    <button onClick = {handleRemove}>Remove</button>
      
     </>
 
   )
 
 }
-=======
-// --> Covered : Immutable States(Completed)
-// --> useEffect all 3 normal variations and 3 clean-up variations
-// --> Data fetch from an API in React
-
-import React, { useState, useEffect } from "react";
-import MyComponent from "./components/MyComponent";
-
-
-export default function App() {
-
-  const [count, setCount] = useState(0);
-  const [visible, setVisible] = useState(true);
-  
-  //With only 1 argument : 
-  useEffect(() => {
-
-    console.log("App wala useEffect is running for count = ", count);
-    
-    return function () {
-      console.log("App wala Clean-up useEffect is running for count" , count);
-    }
-
-  } , [count])
-
-  function handleUpdate() {
-
-    setCount((prev) => {
-      return prev + 1;
-    })
-    
-  }
-
-  function handleBoolean() {
-    setVisible((prev) => {
-      return !prev;
-    })
-  }
-
-  return (
-
-    <>
-      {console.log(" Mycomponent.jsx is rendered inside App.jsx for count = ", count)}
-      { console.log("App.jsx is also rendered becasue of MyComponent re-render")}
-      {visible ? <MyComponent handleUpdate={handleUpdate} count={count} /> : <>Nothing is here !</>}
-      <button onClick={handleUpdate}>Update</button> <br></br> <br></br>
-      <button onClick={handleBoolean}>Un-Mount</button>
-    </>
-    
-  )
-}
-
->>>>>>> 5773b13ffe80170e301bcf69f4715e8306760ad4
