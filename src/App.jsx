eede8cd2426147769a3d360dbfefde296b38c884
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-06 12:54:18 +0530

    fetchAPI call triggered via dispatch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React from "react";
+import {useSelector , useDispatch} from "react-redux";
+import {fetchTodos} from "./feature/Todo/Todo.jsx"
 
 export default function App(){
-      return (
+    
+    const dispatch = useDispatch();
+    
+    function handleClick(){
+        dispatch(fetchTodos())
+    }
+
+    return(
         <>
-        
-        </>
-      )
+        <div>
+        <button onClick={handleClick}>Fetch Todos</button>
+        </div>
+         </>
+    )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit"
-import {todoReducer} from "../feature/Todo/Todo.jsx";
+import todoReducer from "../feature/Todo/Todo.jsx";
 
 
 export const store = configureStore({
-    reducers : {
+    reducer : {
        todo : todoReducer,
     },
 })
