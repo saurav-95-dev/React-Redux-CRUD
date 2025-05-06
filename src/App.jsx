0ca81c281030cd826b56718a07bae6e56ab71c0b
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-06 13:08:58 +0530

    fetch-data to display from API via Redux-Thunk

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,32 @@ import {fetchTodos} from "./feature/Todo/Todo.jsx"
 export default function App(){
     
     const dispatch = useDispatch();
+    const state = useSelector((state) =>{
+        return state;
+    })
+
+    console.log(state);
     
     function handleClick(){
         dispatch(fetchTodos())
+    }
+    
+    if(state.todo.isLoading){
+      return <h1>Load ho rha hai ...</h1>
     }
 
     return(
         <>
         <div>
+
         <button onClick={handleClick}>Fetch Todos</button>
+        
+        {
+           state.todo.data &&  state.todo.data.map((data , index)=>{
+                return <li key={index}>{data.title}</li>
+            })
+        }
+
         </div>
          </>
     )
