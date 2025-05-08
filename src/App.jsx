406bc85561cc66f6335452829909356e896c63f7
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-08 12:22:13 +0530

    React-router-injected

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React, { useEffect  , useState} from "react";
 import Navbar from "./components/Navbar.jsx";
 import "./index.css"
 import Create from "./components/Create.jsx";
-
+import {BrowserRouter , Route , Routes} from "react-router-dom";
 
 export default function App(){
 
     return(
 
         <>
-         <Navbar/>
-         <Create/>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route  exact path = "/" element={<Create/>}/>
+        </Routes>
+        </BrowserRouter>
+      
+         
         </>
 
     )
