63247e12d2dcf957a389fe454fa9293a28bb4d53
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-08 15:22:07 +0530

    Sending data to API via async-Thunk

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,5 +1,5 @@
 import React ,{useState} from "react";
-
+import {useDispatch} from "react-redux"
 
 export default function Create(){ 
 
--- src/features/userDetailsSlice.jsx
+++ src/features/userDetailsSlice.jsx
@@ -1,5 +1,30 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
+
+
+//creating action:
+
+export const createUser = createAsyncThunk("createUser" , async (data ,{rejectWithValue})=>{
+      const response = await fetch("http://681b0fce17018fe50579cc14.mockapi.io/crud-NIC" , {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(data),
+      })
+
+
+      try{
+        const result = await response.json();
+        return result
+      }
+      catch(error){
+        return isRejectedWithValue(error);
+      }
+     
+      
+});
+
 
 export const userDetails = createSlice({
     name : "userDetail",
@@ -7,6 +32,7 @@ export const userDetails = createSlice({
         users : [],
         loading : false,
         error : null,
+        
     },
 
 })
