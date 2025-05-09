9830216a2505ccd7ff5ec7f9684921310989a8fe
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-09 15:10:18 +0530

    Navigation-path set for readAllPost & createPost

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function App(){  
 
@@ -13,12 +14,12 @@ export default function App(){
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link to="/" className="nav-link active" >Create Post </Link> 
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                               <Link to="/read" className="nav-link">All post</Link>
                             </li>
-                            <li className="nav-item dropdown">
+                            {/* <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
                                 </a>
@@ -28,10 +29,8 @@ export default function App(){
                                     <li className="dropdown-divider"></li>
                                     <li><a className="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
+                            </li> */}
+                            
                         </ul>
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
