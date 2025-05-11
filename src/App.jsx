eedf83204516d88742f962679be68984d5287335
Saurabh <devsaurabh086@gmail.com>
2025-05-11 23:57:27 +0530

    Clean-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect  , useState} from "react";
+import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import "./index.css"
 import Create from "./components/Create.jsx";
