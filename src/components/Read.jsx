e3145b5e9cc40617fc726d71e10e426b319762d2
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-13 17:28:29 +0530

    Building Custom-modal

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { showUser } from "../features/userDetailsSlice";
 import CustomModal from "./CustomModal";
 
 export default function Read() {
     const dispatch = useDispatch();
+
+    const [id , setId] = useState();
+    const [showPopup , setShowPopup] = useState(false)
+
     const { users, loading } = useSelector((state) => state.userDetail);
 
     useEffect(() => {
@@ -16,11 +20,11 @@ export default function Read() {
     }
     return (
         <div className="container my-4">
-            <CustomModal/>
+          { showPopup &&  <CustomModal/>}
             <h2 className="text-center mb-4">All Users</h2>
             <div className="row g-4">
                 {users && users.map((ele, index) => (
-                    <div key={index} className="col-12 col-sm-6 col-md-4">
+                    <div key={ele.id} className="col-12 col-sm-6 col-md-4">
                         <div className="card h-100">
                             <div className="card-body">
                                 <h5 className="card-title">{ele.name}</h5>
@@ -28,7 +32,7 @@ export default function Read() {
                                 <p className="card-text">Age: {ele.age}</p>
                                 <p className="card-text">Gender: {ele.gender}</p>
                                 <div className="d-flex flex-column gap-2">
-                                    <a href="#" className="btn btn-outline-info">View</a>
+                                    <button href="#" className="btn btn-outline-info" onClick={()=>{setId(ele.id)}}>View</button>
                                     <a href="#" className="btn btn-outline-warning">Edit</a>
                                     <a href="#" className="btn btn-outline-danger">Delete</a>
                                 </div>
