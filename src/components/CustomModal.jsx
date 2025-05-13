b4f0df06120c46bf48c40286d5466d88584317b4
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-13 17:11:32 +0530

    Navbar-post-count-injected

---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CustomModal =()=>{
+
+    return(
+
+        <div className = "moda=background">
+            <div className="modal-container"></div>
+
+        </div>
+    )
+}
+
+export default CustomModal;
