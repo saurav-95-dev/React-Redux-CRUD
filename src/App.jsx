82f15b57ed21b6e5e31b23b3cb984cfcc7b0cc41
Saurabh Dev <devsaurabh086@gmail.com>
2025-05-15 15:10:22 +0530

    Route added for edit-functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import Create from "./components/Create.jsx";
 import {BrowserRouter , Route , Routes} from "react-router-dom";
 import Read from "./components/Read.jsx";
+import Update from "./components/Update.jsx";
 
 export default function App(){
 
@@ -15,6 +16,7 @@ export default function App(){
         <Routes>
             <Route  exact path = "/" element={<Create/>}/>
             <Route  exact path = "/read" element={<Read/>}/>
+            <Route  exact path="/edit:id" element={<Update/>}/>
         </Routes>
         </BrowserRouter>
       
