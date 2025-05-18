33f461d43f67fc17305b37e722539f87769ce1d9
Saurabh <devsaurabh086@gmail.com>
2025-05-18 23:49:31 +0530

    Capturing data via useSelector from Redux store

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 
 const Update = () => {
+
+    //Capturing the id from the URL using useParams:
+    const { id } = useParams();
+
   
 
   return (
