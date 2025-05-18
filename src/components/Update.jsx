7ab9a039e072f080ab97d28d9ea029038009c45f
Saurabh <devsaurabh086@gmail.com>
2025-05-18 23:33:46 +0530

    Update.jsx refined

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,13 +1,67 @@
 import React from "react";
 
-export default function Update(){
 
-    return(
-        
-        <>
-        <div>Update</div>
-        </>
+const Update = () => {
+  
 
-    )
+  return (
+    <div>
+      <h2 className="my-2">Edit the data</h2>
+      <form className="w-50 mx-auto my-5" >
+        <div class="mb-3">
+          <label class="form-label">Name</label>
+          <input
+            type="text"
+            name="name"
+            class="form-control"
+           
+          />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            class="form-control"
+         
+          />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Age</label>
+          <input
+            type="text"
+            name="age"
+            class="form-control"
+            
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="form-check-input"
+            name="gender"
+            value="Male"
+            type="radio"
+           
+          />
+          <label class="form-check-label">Male</label>
+        </div>
+        <div class="mb-3">
+          <input
+            class="form-check-input"
+            name="gender"
+            value="Female"
+            type="radio"
+           
+          />
+          <label class="form-check-label">Female</label>
+        </div>
 
-}
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Update;
